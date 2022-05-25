76fd26c6aa20edc8288c050b5e0a0ca6e76fcdac
Daikeri <kotdmitrii65@gmail.com>
2022-05-25 10:30:05 +0300

    :+1:

--- LR_11.pl
+++ LR_11.pl
@@ -83,3 +83,11 @@ countDigD(X,Y,Count):- X>0,!,
     (P>3,P mod 2 =\= 0 -> Count1 is Count + 1; Count1 is Count),
     countDigD(X1,Y,Count1).
 countDigD(_,Y,Z):- Y is Z.
+
+%Number_19.7
+fib(0,1):-!.
+fib(1,1):-!.
+fib(N,X):-N>1,
+    N1 is N-1, N2 is N-2,
+    fib(N1,X1),fib(N2,X2),
+    X is X1+X2.
